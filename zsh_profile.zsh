62fee36a3b6cdae5d181deda29722f82f904c250
Lorenzo <lorenzobertoni9@gmail.com>
2020-12-04 17:25:18 +0100

    add path to python3

--- zsh_profile.zsh
+++ zsh_profile.zsh
@@ -1,4 +1,5 @@
 #Virtualenvwrapper
+#export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3  # For local without python2
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Devel
 #source /usr/local/bin/virtualenvwrapper.sh # Only for local
